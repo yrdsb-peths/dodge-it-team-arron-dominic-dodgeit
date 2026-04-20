4cf178c0d496e7c46d91a0aa6f661868fcc08ba9
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-20 11:36:35 -0400

    create the hero class

---
+++ HeroMaybe.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HeroMaybe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HeroMaybe extends Actor
+{
+    /**
+     * Act - do whatever the HeroMaybe wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
